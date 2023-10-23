a66348f34afb14495f3a129e737bfd2fae0fe9e2
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-10-22 22:35:44 -0400

    Pushing my skeleton app. (Very ominous and halloween esque commit, I know. )
    I followed a guide to make a to-do list app, but I didn't want to just copy the code line for line, so I went off and made my own features from scratch. I created a navigation bar with two pages, a settings page and a task list page. The task list page does not work in this version because I ran out of time unfortunately but I wanted to push this branch anyways since no one else made a branch yet.

---
+++ Project/tinyTask/components/TaskList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TaskList = () => {
+    return (
+        <View>
+            <Text>Task List Screen</Text>
+        </View>
+    );
+};
+
+export default TaskList;
