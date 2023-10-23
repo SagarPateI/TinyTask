2e5201ed0e7c36970178b2e4a9e3082953c99d38
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-10-23 16:18:23 -0400

    Cleaned up the code and added a navigation bar that reflects the wireframe from Thursday

---
+++ Project/tinyTask/TaskListScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function TaskListScreen() {
+  return (
+    <View>
+      <Text>Task List Screen Screen</Text>
+    </View>
+  );
+}
