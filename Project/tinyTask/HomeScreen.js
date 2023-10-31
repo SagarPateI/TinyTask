fed62e6d10f23483cc4062c1c1896d11bacf6a3a
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-10-31 01:09:15 -0400

    Added some very basic functionality for the overview page and fixed the Tasks.js code

--- Project/tinyTask/HomeScreen.js
+++ Project/tinyTask/HomeScreen.js
@@ -1,25 +1,108 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Text } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons"; // Import icons from Expo package
 
-export default function HomeScreen() {
+const HomeScreen = () => {
+  const currentDate = new Date().toDateString(); // Get the current date
+
+  // Placeholder event handlers for icon press
+  const handleProfileIconPress = () => {
+    // Handle profile icon press
+  };
+
+  const handleHomeIconPress = () => {
+    // Handle Home icon press
+  };
+
+  const handleTasksNavigationPress = () => {
+    // Handle Tasks navigation press
+  };
+
+  const handleCalendarNavigationPress = () => {
+    // Handle Calendar navigation press
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.h1}>Home Screen</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      {/* Welcoming Interface */}
+      <View style={styles.welcomeSection}>
+        <Text style={styles.welcomeText}>Welcome, User</Text>
+        <Text style={styles.dateText}>{currentDate}</Text>
+      </View>
+
+      {/* Profile Icon */}
+      <TouchableOpacity
+        style={styles.profileIcon}
+        onPress={handleProfileIconPress}
+      >
+        <MaterialIcons name="account-circle" size={30} color="#555" />
+      </TouchableOpacity>
+
+      {/* Events Section */}
+      <View style={styles.section}>
+        {/* Display upcoming events */}
+        {/* ... (code for displaying events) */}
+      </View>
+
+      {/* Tasks Section */}
+      <View style={styles.section}>
+        {/* Display tasks with checkboxes */}
+        {/* ... (code for displaying tasks) */}
+      </View>
+
+      {/* Navigation Bar */}
+      <View style={styles.navigationBar}>
+        <TouchableOpacity onPress={handleHomeIconPress}>
+          <MaterialIcons name="home" size={30} color="#555" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleTasksNavigationPress}>
+          <MaterialIcons name="check" size={30} color="#555" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleCalendarNavigationPress}>
+          <MaterialIcons name="date-range" size={30} color="#555" />
+        </TouchableOpacity>
+      </View>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#ecf0f1",
-    padding: 8,
+    backgroundColor: "#fff",
+    padding: 20,
   },
-  h1: {
-    margin: 28,
-    fontSize: 36,
+  welcomeSection: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  welcomeText: {
+    fontSize: 24,
     fontWeight: "bold",
-    textAlign: "center",
+  },
+  dateText: {
+    fontSize: 16,
+    color: "#555",
+  },
+  profileIcon: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+  },
+  section: {
+    marginTop: 20,
+    // Add styles for event and tasks sections
+  },
+  navigationBar: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    backgroundColor: "#eee",
+    paddingVertical: 10,
   },
 });
+
+export default HomeScreen;
--- Project/tinyTask/Task.js
+++ Project/tinyTask/Task.js
@@ -1,51 +1,64 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
 
 const Task = (props) => {
-    return (
-        <View style={styles.item}>
-            <View style={styles.itemLeft}>
-                <View style={styles.square}></View>
-                <Text style={styles.itemText}>{props.text}</Text>
-            </View>
-            <View style={styles.circular}></View>
-        </View>
-    );
+  const { text, backgroundColor } = props; // Added prop for customizable background color
+
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={[styles.square, { backgroundColor }]}></View>{" "}
+        {/* Use the custom background color */}
+        <Text style={styles.itemText}>{text}</Text>
+      </View>
+      <View style={styles.circular}></View>
+    </View>
+  );
+};
+
+Task.propTypes = {
+  text: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string, // Define prop types
+};
+
+Task.defaultProps = {
+  backgroundColor: "#55BCF6", // Default background color
 };
 
 const styles = StyleSheet.create({
-    item: {
-        backgroundColor: "#FFF",
-        padding: 15,
-        borderRadius: 10,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginBottom: 20,
-    },
-    itemLeft: {
-        flexDirection: "row",
-        alignItems: "center",
-        flexWrap: "wrap",
-    },
-    square: {
-        width: 24,
-        height: 24,
-        backgroundColor: "#55BCF6",
-        opacity: 0.4,
-        borderRadius: 5,
-        marginRight: 15,
-    },
-    itemText: {
-        maxWidth: "80%",
-    },
-    circular: {
-        width: 12,
-        height: 12,
-        borderColor: "#55BCF6",
-        borderWidth: 2,
-        borderRadius: 5,
-    },
+  item: {
+    backgroundColor: "#FFF",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#55BCF6",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: "80%",
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: "#55BCF6",
+    borderWidth: 2,
+    borderRadius: 5,
+  },
 });
 
 export default Task;
