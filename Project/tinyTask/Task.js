b5f41ff773c54c99cd98ac0af9174acb550483b4
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-10-31 00:28:05 -0400

    Rolling back all changes to Task.js to the last working version. I cannot resolve the bugs.

--- Project/tinyTask/Task.js
+++ Project/tinyTask/Task.js
@@ -1,28 +1,16 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import PropTypes from "prop-types";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const Task = (props) => {
-    const { text, backgroundColor } = props; // Added prop for customizable background color
-
     return (
         <View style={styles.item}>
             <View style={styles.itemLeft}>
-                <View style={[styles.square, { backgroundColor }]}></View> {/* Use the custom background color */}
-                <Text style={styles.itemText}>{text}</Text>
+                <View style={styles.square}></View>
+                <Text style={styles.itemText}>{props.text}</Text>
             </View>
             <View style={styles.circular}></View>
         </View>
     );
-};
-
-Task.propTypes = {
-    text: PropTypes.string.isRequired,
-    backgroundColor: PropTypes.string, // Define prop types
-};
-
-Task.defaultProps = {
-    backgroundColor: "#55BCF6", // Default background color
 };
 
 const styles = StyleSheet.create({
