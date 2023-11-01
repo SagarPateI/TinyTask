59ce7a9e8de00c2f66a15b63eb460e5f898c8704
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-01 16:24:44 -0400

    Re-created the project files and reset the gitignore

--- Project/tinytask/App.js
+++ Project/tinytask/App.js
@@ -1,47 +1,20 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MaterialCommunityIcons } from "react-native-vector-icons";
-import HomeScreen from "./HomeScreen";
-import TaskListScreen from "./TaskListScreen";
-import CalendarViewScreen from "./CalendarViewScreen";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const Tab = createBottomTabNavigator();
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons name="home" color={color} size={size} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Task List"
-            component={TaskListScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons name="check" color={color} size={size} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Calendar View"
-            component={CalendarViewScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons name="calendar" color={color} size={size} />
-              ),
-            }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
