ab7ba6eea35bb7759d550432c334395c51604d28
sdp2391@gmail.com <sdp2391@gmail.com>
2023-11-04 17:27:58 -0400

    Successfully created the basic pages in typescript.

---
+++ Project/tinytask/app/(tabs)/TaskListScreen.tsx
@@ -0,0 +1,13 @@
+// app/tabs/TaskListScreen.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TaskListScreen = () => {
+  return (
+    <View>
+      <Text>Task List Screen</Text>
+    </View>
+  );
+};
+
+export default TaskListScreen;
