374b94c09b539aecb6ff35332f260401b1eb81c4
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-04 18:00:57 -0400

    Got the task list working in the typescript version

--- Project/tinytask/app/navigation/TabNavigator.tsx
+++ Project/tinytask/app/navigation/TabNavigator.tsx
@@ -1,35 +1,20 @@
-// app/navigation/TabNavigator.tsx
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
+// tinytask/app/navigation/TabNavigator.tsx
 
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../(tabs)/HomeScreen';
 import TaskListScreen from '../(tabs)/TaskListScreen';
 import CalendarScreen from '../(tabs)/CalendarScreen';
 
 const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
-
-const MainStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="TaskList" component={TaskListScreen} />
-      <Stack.Screen name="Calendar" component={CalendarScreen} />
-    </Stack.Navigator>
-  );
-};
 
 const TabNavigator = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={MainStack} />
-        <Tab.Screen name="TaskList" component={TaskListScreen} />
-        <Tab.Screen name="Calendar" component={CalendarScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="TaskList" component={TaskListScreen} />
+      <Tab.Screen name="Calendar" component={CalendarScreen} />
+    </Tab.Navigator>
   );
 };
 
