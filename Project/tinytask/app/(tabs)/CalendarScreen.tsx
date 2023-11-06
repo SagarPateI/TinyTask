794b1526c0254bda8ff3202fd191d244f760b629
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-06 11:16:11 -0500

    Rolling back some of the calendar view changes because I can't figure out the Agenda feature yet.

--- Project/tinytask/app/(tabs)/CalendarScreen.tsx
+++ Project/tinytask/app/(tabs)/CalendarScreen.tsx
@@ -16,22 +16,15 @@ const CalendarScreen = () => {
     // perform actions when a day is selected
   };
 
-  return (
+return (
     <View style={{ flex: 1, backgroundColor }}>
-      <View style={{ flex: 1, backgroundColor }}>
-        <Calendar
-          onDayPress={onDayPress}
-          markedDates={{
-            // mark a specific date
-            "2023-11-08": {
-              selected: true,
-              marked: true,
-              selectedColor: "blue",
-            },
-          }}
-          style={{ backgroundColor }} // Optional: Some libraries might allow setting a background color directly
-        />
-      </View>
+      <Calendar
+        onDayPress={onDayPress}
+        markedDates={{
+          // mark a specific date
+          '2023-11-08': { selected: true, marked: true, selectedColor: 'blue' },
+        }}
+      />
     </View>
   );
 };
