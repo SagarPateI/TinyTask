9fbc95c867f4742de13fcca786a2e6ac19add8f5
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-06 10:54:37 -0500

    Added separate colors for the backgrounds of each page.

--- Project/tinytask/app/(tabs)/CalendarScreen.tsx
+++ Project/tinytask/app/(tabs)/CalendarScreen.tsx
@@ -1,23 +1,37 @@
-// app/tabs/CalendarScreen.tsx
-import React from 'react';
-import { View } from 'react-native';
-import { Calendar } from 'react-native-calendars';
+import React from "react";
+import { Calendar } from "react-native-calendars";
+import { useThemeColor, View } from "../../components/Themed";
 
 const CalendarScreen = () => {
+  const backgroundColor = useThemeColor(
+    {
+      light: "#f0f0f0", // Light gray background color
+      dark: "#333333", // Dark gray background color
+    },
+    "background"
+  );
+
   const onDayPress = (day: any) => {
-    console.log('selected day', day);
+    console.log("selected day", day);
     // perform actions when a day is selected
   };
 
   return (
-    <View style={{ flex: 1 }}>
-      <Calendar
-        onDayPress={onDayPress}
-        markedDates={{
-        // mark a specific date
-          '2023-11-08': { selected: true, marked: true, selectedColor: 'blue' },
-        }}
-      />
+    <View style={{ flex: 1, backgroundColor }}>
+      <View style={{ flex: 1, backgroundColor }}>
+        <Calendar
+          onDayPress={onDayPress}
+          markedDates={{
+            // mark a specific date
+            "2023-11-08": {
+              selected: true,
+              marked: true,
+              selectedColor: "blue",
+            },
+          }}
+          style={{ backgroundColor }} // Optional: Some libraries might allow setting a background color directly
+        />
+      </View>
     </View>
   );
 };
