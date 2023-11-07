9c1ae999661e5b4846e612e7cde5835ceedbe9a6
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-07 10:17:51 -0500

    The navigation bug is fixed (for now). It's related to the Typescript file names. Expo is looking for index.tsx by default and this is not negotiable as far as I know.

--- Project/tinytask/app/[...missing].tsx
+++ Project/tinytask/app/[...missing].tsx
@@ -10,6 +10,7 @@ export default function NotFoundScreen() {
       <View style={styles.container}>
         <Text style={styles.title}>This screen doesn't exist.</Text>
 
+        {/*  <EditScreenInfo path="app/(tabs)/HomeScreen.tsx" /> */}
         <Link href="/" style={styles.link}>
           <Text style={styles.linkText}>Go to home screen!</Text>
         </Link>
