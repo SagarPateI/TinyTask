67be39ca27c3ed20d2527f78d9e108b882506cd2
sdp2391@gmail.com <sdp2391@gmail.com>
2023-11-08 00:08:36 -0500

    Major structural reconfiguration of the code for the purpose of bugfixing. It seems that the builds aren't crashing anymore. This is the new v0.1.2

--- Project/tinytask/components/Task.tsx
+++ Project/tinytask/components/Task.tsx
@@ -1,56 +1,69 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import {
+  useThemeColor,
+  Text as ThemedText,
+  View as ThemedView,
+} from "./Themed";
 
 interface TaskProps {
-    text: string;
-    backgroundColor?: string;
+  text: string;
+  backgroundColor?: string;
 }
 
-const Task: React.FC<TaskProps> = ({ text, backgroundColor = "#55BCF6" }) => {
-    return (
-        <View style={styles.item}>
-            <View style={styles.itemLeft}>
-                <View style={[styles.square, { backgroundColor }]}></View>
-                <Text style={styles.itemText}>{text}</Text>
-            </View>
-            <View style={styles.circular}></View>
-        </View>
-    );
-};
+const Task: React.FC<TaskProps> = ({ text }) => {
+  const backgroundColor = useThemeColor(
+    {
+      light: "#ffffff",
+      dark: "#000000",
+    },
+    "background"
+  );
 
-const styles = StyleSheet.create({
+  const styles = StyleSheet.create({
     item: {
-        backgroundColor: "#FFF",
-        padding: 15,
-        borderRadius: 10,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginBottom: 20,
+      backgroundColor,
+      padding: 15,
+      borderRadius: 10,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      marginBottom: 20,
     },
     itemLeft: {
-        flexDirection: "row",
-        alignItems: "center",
-        flexWrap: "wrap",
+      flexDirection: "row",
+      alignItems: "center",
+      flexWrap: "wrap",
     },
     square: {
-        width: 24,
-        height: 24,
-        backgroundColor: "#55BCF6",
-        opacity: 0.4,
-        borderRadius: 5,
-        marginRight: 15,
+      width: 24,
+      height: 24,
+      backgroundColor: "#55BCF6",
+      opacity: 0.4,
+      borderRadius: 5,
+      marginRight: 15,
     },
     itemText: {
-        maxWidth: "80%",
+      maxWidth: "80%",
     },
     circular: {
-        width: 12,
-        height: 12,
-        borderColor: "#55BCF6",
-        borderWidth: 2,
-        borderRadius: 5,
+      width: 12,
+      height: 12,
+      borderColor: "#55BCF6",
+      borderWidth: 2,
+      borderRadius: 5,
     },
-});
+  });
+
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={styles.square}></View>
+        <ThemedText style={styles.itemText}>{text}</ThemedText>
+      </View>
+      <View style={styles.circular}></View>
+    </View>
+  );
+};
 
 export default Task;
