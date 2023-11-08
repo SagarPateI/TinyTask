68fe1850b02a3f9f82c25ac3a348183126350adb
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-08 00:08:36 -0500

    Major structural reconfiguration of the code for the purpose of bugfixing. It seems that the builds aren't crashing anymore. This is the new v0.1.2

--- Project/tinytask/app/(tabs)/TaskListScreen.tsx
+++ Project/tinytask/app/(tabs)/TaskListScreen.tsx
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
 import {
+  Keyboard,
   KeyboardAvoidingView,
   StyleSheet,
-  SafeAreaView,
   Text,
   View,
   TextInput,
   TouchableOpacity,
-  Keyboard,
-  ScrollView,
   Platform,
+  ScrollView,
 } from "react-native";
-import Task from '../../components/Task';
+import Task from "../../components/Task";
+import {
+  Text as ThemedText,
+  View as ThemedView,
+  useThemeColor,
+} from "../../components/Themed";
 
 const TaskListScreen: React.FC = () => {
   const [task, setTask] = useState<string>("");
   const [taskItems, setTaskItems] = useState<string[]>([]);
+
+  const backgroundColor = useThemeColor(
+    {
+      light: "#FFFFFF", // Light gray background color
+      dark: "#121212", // Dark gray background color
+    },
+    "background"
+  );
 
   const handleAddTask = () => {
     Keyboard.dismiss();
@@ -31,14 +43,67 @@ const TaskListScreen: React.FC = () => {
     setTaskItems(itemsCopy);
   };
 
+  const textColor = useThemeColor({}, "text");
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingHorizontal: 20,
+      backgroundColor,
+    },
+    tasksWrapper: {
+      paddingTop: 80,
+    },
+    sectionTitle: {
+      fontSize: 24,
+      fontWeight: "bold",
+      color: textColor,
+    },
+    items: {
+      marginTop: 30,
+    },
+    writeTaskWrapper: {
+      position: "absolute",
+      bottom: 60,
+      width: "100%",
+      flexDirection: "row",
+      justifyContent: "space-around",
+      alignItems: "center",
+    },
+    input: {
+      paddingVertical: 15,
+      paddingHorizontal: 15,
+      borderRadius: 60,
+      borderColor: "#C0C0C0",
+      borderWidth: 1,
+      width: 250,
+      color: textColor, // Applying Themed color
+    },
+    addWrapper: {
+      width: 60,
+      height: 60,
+      borderRadius: 60,
+      justifyContent: "center",
+      alignItems: "center",
+      borderColor: "#C0C0C0",
+      borderWidth: 1,
+    },
+    addText: {
+      color: textColor,
+    },
+    scrollContainer: {
+      flexGrow: 1,
+    },
+  });
+
   return (
-    <View style={styles.container}>
+    <ThemedView style={styles.container}>
       <ScrollView
         contentContainerStyle={styles.scrollContainer}
         keyboardShouldPersistTaps="handled"
       >
         <View style={styles.tasksWrapper}>
-          <Text style={styles.sectionTitle}>Today's tasks</Text>
+          <ThemedText style={styles.sectionTitle}>Today's tasks</ThemedText>
           <View style={styles.items}>
             {taskItems.map((item, index) => (
               <TouchableOpacity key={index} onPress={() => completeTask(index)}>
@@ -56,6 +121,7 @@ const TaskListScreen: React.FC = () => {
         <TextInput
           style={styles.input}
           placeholder={"Write a task"}
+          placeholderTextColor={textColor}
           value={task}
           onChangeText={(text) => setTask(text)}
         />
@@ -65,57 +131,8 @@ const TaskListScreen: React.FC = () => {
           </View>
         </TouchableOpacity>
       </KeyboardAvoidingView>
-    </View>
+    </ThemedView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#E8EAED",
-  },
-  scrollContainer: {
-    flexGrow: 1,
-  },
-  tasksWrapper: {
-    paddingTop: 80,
-    paddingHorizontal: 20,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  items: {
-    marginTop: 30,
-  },
-  writeTaskWrapper: {
-    position: "absolute",
-    bottom: 60,
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-  },
-  input: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    backgroundColor: "#FFF",
-    borderRadius: 60,
-    borderColor: "#C0C0C0",
-    borderWidth: 1,
-    width: 250,
-  },
-  addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: "#FFF",
-    borderRadius: 60,
-    justifyContent: "center",
-    alignItems: "center",
-    borderColor: "#C0C0C0",
-    borderWidth: 1,
-  },
-  addText: {},
-});
 
 export default TaskListScreen;
