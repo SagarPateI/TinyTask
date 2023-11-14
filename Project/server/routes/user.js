369c76e07251fe7226a64d4834cdbf487db5c424
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-14 10:30:09 -0500

    Added the server project from Cris' branch before I start adding the login pages.

---
+++ Project/server/routes/user.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+
+
+const router = express.Router();
+
+
+const {
+    createUser,
+    userLogin,
+} = require('../controllers/user');
+
+
+router.post("/signup", createUser);
+router.post("/login", userLogin);
+module.exports = router;
