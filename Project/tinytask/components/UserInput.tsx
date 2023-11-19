49fedc395983e9bd108e6833f17707089fe8b4ce
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-19 18:22:47 -0500

    Improved the quickstart guide, converted SubmitButton and UserInput to typescript and also found substitutions for the unsupported libraries that were preventing the conversion from being done originally. Also changed some of the components of the project to improve support

---
+++ Project/tinytask/components/UserInput.tsx
@@ -0,0 +1,60 @@
+import React, { FC } from "react";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  TextInputProps,
+  Text,
+} from "react-native";
+
+{
+  /* ACCESSING AND SENDING VALUES TO TEXT INPUT*/
+}
+interface UserInputProps extends TextInputProps {
+  name: string;
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const UserInput: FC<UserInputProps> = ({
+  name,
+  value,
+  setValue,
+  autoCapitalize = "none",
+  keyboardType = "default",
+  secureTextEntry = false,
+}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{name}</Text>
+      <TextInput
+        autoCorrect={false}
+        autoCapitalize={autoCapitalize}
+        keyboardType={keyboardType}
+        secureTextEntry={secureTextEntry}
+        style={styles.input}
+        value={value}
+        onChangeText={(text) => setValue(text)}
+      />
+      {/* USER INPUT (TEXT) WILL BE SENT TO SETVALUE FUNCTION TO SET VALUE FOR THE STATE*/}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+  },
+  label: {
+    color: "#FFFFFF",
+  },
+  input: {
+    borderBottomWidth: 0.5,
+    height: 48,
+    borderBottomColor: "#8e93a1",
+    marginBottom: 30,
+    color: "#ffffff",
+  },
+});
+
+export default UserInput;
