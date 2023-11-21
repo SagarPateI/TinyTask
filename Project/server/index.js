7074c3cf6725022a25757c1b0b4f5c359e3a5eca
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-20 22:54:04 -0500

    Cleaning up the code

--- Project/server/index.js
+++ Project/server/index.js
@@ -8,14 +8,12 @@ const authRouter = require('./routes/user');
 const http = require("http").createServer(app);
 const port = 8000;
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(authRouter);
 
-
 //listen = express function
 http.listen(port, () => {
-    console.log('Listening at http://localhost:',port);
+    console.log('Listening at http://localhost:', port);
 });
