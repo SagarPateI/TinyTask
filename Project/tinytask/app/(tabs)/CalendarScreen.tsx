22f555147dfd812b36dfdc40832954ad76a78751
Alexis <145714043+ChicoQuemador1@users.noreply.github.com>
2023-11-22 01:15:53 -0500

    timeline shit
    Original timeline stuff. The hardcoded events work but I cant add an event from the timeline

--- Project/tinytask/app/(tabs)/CalendarScreen.tsx
+++ Project/tinytask/app/(tabs)/CalendarScreen.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { useState } from "react";
 import {
   View,
@@ -156,3 +157,268 @@ const styles = StyleSheet.create({
 });
 
 export default CalendarScreen;
+=======
+import groupBy from 'lodash/groupBy';
+import filter from 'lodash/filter';
+import find from 'lodash/find';
+
+import React, {Component} from 'react';
+import {Alert} from 'react-native';
+import {ExpandableCalendar, TimelineEventProps, TimelineList, CalendarProvider, TimelineProps, CalendarUtils} from 'react-native-calendars';
+
+
+
+const EVENT_COLOR = '#e6add8';
+const today = new Date();
+export const getDate = (offset = 0) => CalendarUtils.getCalendarDateString(new Date().setDate(today.getDate() + offset));
+
+// this is mock data THAT IS HARD CODED
+export const timelineEvents: TimelineEventProps[] = [
+  {
+    start: `${getDate(-1)} 09:20:00`,
+    end: `${getDate(-1)} 12:00:00`,
+    title: 'Merge Request to React Native Calendars',
+    summary: 'Merge Timeline Calendar to React Native Calendars'
+  },
+  {
+    start: `${getDate()} 01:15:00`,
+    end: `${getDate()} 02:30:00`,
+    title: 'Meeting A',
+    summary: 'Summary for meeting A',
+    color: EVENT_COLOR
+  },
+  {
+    start: `${getDate()} 01:30:00`,
+    end: `${getDate()} 02:30:00`,
+    title: 'Meeting B',
+    summary: 'Summary for meeting B',
+    color: EVENT_COLOR
+  },
+  {
+    start: `${getDate()} 01:45:00`,
+    end: `${getDate()} 02:45:00`,
+    title: 'Meeting C',
+    summary: 'Summary for meeting C',
+    color: EVENT_COLOR
+  },
+  {
+    start: `${getDate()} 02:40:00`,
+    end: `${getDate()} 03:10:00`,
+    title: 'Meeting D',
+    summary: 'Summary for meeting D',
+    color: EVENT_COLOR
+  },
+  {
+    start: `${getDate()} 02:50:00`,
+    end: `${getDate()} 03:20:00`,
+    title: 'Meeting E',
+    summary: 'Summary for meeting E',
+    color: EVENT_COLOR
+  },
+  {
+    start: `${getDate()} 04:30:00`,
+    end: `${getDate()} 05:30:00`,
+    title: 'Meeting F',
+    summary: 'Summary for meeting F',
+    color: EVENT_COLOR
+  },
+  {
+    start: `${getDate(1)} 00:30:00`,
+    end: `${getDate(1)} 01:30:00`,
+    title: 'Visit Grand Mother',
+    summary: 'Visit Grand Mother and bring some fruits.',
+    color: 'lightblue'
+  },
+  {
+    start: `${getDate(1)} 02:30:00`,
+    end: `${getDate(1)} 03:20:00`,
+    title: 'Meeting with Prof. Behjet Zuhaira',
+    summary: 'Meeting with Prof. Behjet at 130 in her office.',
+    color: EVENT_COLOR
+  },
+  {
+    start: `${getDate(1)} 04:10:00`,
+    end: `${getDate(1)} 04:40:00`,
+    title: 'Tea Time with Dr. Hasan',
+    summary: 'Tea Time with Dr. Hasan, Talk about Project'
+  },
+  {
+    start: `${getDate(1)} 01:05:00`,
+    end: `${getDate(1)} 01:35:00`,
+    title: 'Dr. Mariana Joseph',
+    summary: '3412 Piedmont Rd NE, GA 3032'
+  },
+  {
+    start: `${getDate(1)} 14:30:00`,
+    end: `${getDate(1)} 16:30:00`,
+    title: 'Meeting Some Friends in ARMED',
+    summary: 'Arsalan, Hasnaat, Talha, Waleed, Bilal',
+    color: 'pink'
+  },
+  {
+    start: `${getDate(2)} 01:40:00`,
+    end: `${getDate(2)} 02:25:00`,
+    title: 'Meet Sir Khurram Iqbal',
+    summary: 'Computer Science Dept. Comsats Islamabad',
+    color: 'orange'
+  },
+  {
+    start: `${getDate(2)} 04:10:00`,
+    end: `${getDate(2)} 04:40:00`,
+    title: 'Tea Time with Colleagues',
+    summary: 'WeRplay'
+  },
+  {
+    start: `${getDate(2)} 00:45:00`,
+    end: `${getDate(2)} 01:45:00`,
+    title: 'Lets Play Apex Legends',
+    summary: 'with Boys at Work'
+  },
+  {
+    start: `${getDate(2)} 11:30:00`,
+    end: `${getDate(2)} 12:30:00`,
+    title: 'Dr. Mariana Joseph',
+    summary: '3412 Piedmont Rd NE, GA 3032'
+  },
+  {
+    start: `${getDate(4)} 12:10:00`,
+    end: `${getDate(4)} 13:45:00`,
+    title: 'Merge Request to React Native Calendars',
+    summary: 'Merge Timeline Calendar to React Native Calendars'
+  }
+];
+
+
+const INITIAL_TIME = {hour: 9, minutes: 0};
+const EVENTS: TimelineEventProps[] = timelineEvents;
+
+export default class TimelineCalendarScreen extends Component {
+  state = {
+    currentDate: getDate(),
+    events: EVENTS,
+    eventsByDate: groupBy(EVENTS, e => CalendarUtils.getCalendarDateString(e.start)) as {
+      [key: string]: TimelineEventProps[];
+    }
+  };
+
+  marked = {
+    [`${getDate(-1)}`]: {marked: true},
+    [`${getDate()}`]: {marked: true},
+    [`${getDate(1)}`]: {marked: true},
+    [`${getDate(2)}`]: {marked: true},
+    [`${getDate(4)}`]: {marked: true}
+  };
+
+  onDateChanged = (date: string, source: string) => {
+    console.log('TimelineCalendarScreen onDateChanged: ', date, source);
+    this.setState({currentDate: date});
+  };
+
+  onMonthChange = (month: any, updateSource: any) => {
+    console.log('TimelineCalendarScreen onMonthChange: ', month, updateSource);
+  };
+
+
+
+  // this does not work adding from the screen yet I DONT KNOW WHYY
+  createNewEvent: TimelineProps['onBackgroundLongPress'] = (timeString, timeObject) => {
+    const {eventsByDate} = this.state;
+    const hourString = `${(timeObject.hour + 1).toString().padStart(2, '0')}`;
+    const minutesString = `${timeObject.minutes.toString().padStart(2, '0')}`;
+
+    const newEvent = {
+      id: 'draft',
+      start: `${timeString}`,
+      end: `${timeObject.date} ${hourString}:${minutesString}:00`,
+      title: 'New Event',
+      color: 'white'
+    };
+
+    if (timeObject.date) {
+      if (eventsByDate[timeObject.date]) {
+        eventsByDate[timeObject.date] = [...eventsByDate[timeObject.date], newEvent];
+        this.setState({eventsByDate});
+      } else {
+        eventsByDate[timeObject.date] = [newEvent];
+        this.setState({eventsByDate: {...eventsByDate}});
+      }
+    }
+  };
+
+  approveNewEvent: TimelineProps['onBackgroundLongPressOut'] = (_timeString, timeObject) => {
+    const {eventsByDate} = this.state;
+
+    Alert.prompt('New Event', 'Enter event title', [
+      {
+        text: 'Cancel',
+        onPress: () => {
+          if (timeObject.date) {
+            eventsByDate[timeObject.date] = filter(eventsByDate[timeObject.date], e => e.id !== 'draft');
+
+            this.setState({
+              eventsByDate
+            });
+          }
+        }
+      },
+      {
+        text: 'Create',
+        onPress: eventTitle => {
+          if (timeObject.date) {
+            const draftEvent = find(eventsByDate[timeObject.date], {id: 'draft'});
+            if (draftEvent) {
+              draftEvent.id = undefined;
+              draftEvent.title = eventTitle ?? 'New Event';
+              draftEvent.color = 'lightgreen';
+              eventsByDate[timeObject.date] = [...eventsByDate[timeObject.date]];
+
+              this.setState({
+                eventsByDate
+              });
+            }
+          }
+        }
+      }
+    ]);
+  };
+
+
+
+  private timelineProps: Partial<TimelineProps> = {
+    format24h: false,
+    unavailableHours: [{start: 0, end: 6}, {start: 22, end: 24}],
+    onBackgroundLongPress: this.createNewEvent,
+    onBackgroundLongPressOut: this.approveNewEvent,
+    overlapEventsSpacing: 8,
+    rightEdgeSpacing: 24,
+  };
+
+  render() {
+    const {currentDate, eventsByDate} = this.state;
+
+    return (
+      <CalendarProvider
+        date={currentDate}
+        onDateChanged={this.onDateChanged}
+        onMonthChange={this.onMonthChange}
+        showTodayButton
+        disabledOpacity={0.6}
+      >
+        <ExpandableCalendar
+          firstDay={1}
+          hideArrows 
+          markedDates={this.marked}
+        />
+        <TimelineList
+          events={eventsByDate}
+          timelineProps={this.timelineProps}
+          showNowIndicator
+  
+          scrollToFirst
+          initialTime={INITIAL_TIME}
+        />
+      </CalendarProvider>
+    );
+  }
+}
+>>>>>>> Stashed changes
