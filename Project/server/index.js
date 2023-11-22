43e7f6040dda2b0ee9f29aed7bebafd4eff2a5db
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-22 15:53:39 -0500

    Updating the index.js file in the server

--- Project/server/index.js
+++ Project/server/index.js
@@ -7,13 +7,34 @@ const app = express();
 const authRouter = require('./routes/user');
 const tasksRouter = require('./routes/tasks'); // Import the tasks router
 const http = require("http").createServer(app);
-const port = 8000;
+const port = process.env.PORT || 8000;
+const mongoose = require('mongoose');
+
+// MongoDB connection setup
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => console.log('Connected to MongoDB'));
+
+app.use(express.json());
+app.use(cors());
+
+// User routes
+const authRouter = require('./routes/user');
+app.use('/auth', authRouter);
+
+// Task routes
+const taskRouter = require('./routes/tasks');
+app.use('/tasks', taskRouter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(authRouter);
-app.use('/tasks', tasksRouter); // Use the tasks router for '/tasks' endpoints
+//app.use(authRouter);
 
 //listen = express function
 http.listen(port, () => {
