761dde941b794368b6cc629986ce2d72ec3a7a23
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-22 15:55:35 -0500

    Fixed the sloppy index.js server code.

--- Project/server/index.js
+++ Project/server/index.js
@@ -1,26 +1,16 @@
 require('dotenv').config();
 require('./models/database');
-const cors = require("cors");
+const cors = require('cors');
 const express = require('express');
-const User = require('./models/user');
 const app = express();
-const authRouter = require('./routes/user');
-const tasksRouter = require('./routes/tasks'); // Import the tasks router
-const http = require("http").createServer(app);
+const http = require('http').createServer(app);
 const port = process.env.PORT || 8000;
-const mongoose = require('mongoose');
 
-// MongoDB connection setup
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => console.log('Connected to MongoDB'));
+const User = require('./models/user');
+const TaskRouter = require('./routes/tasks'); // Importing task routes
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // User routes
@@ -28,15 +18,8 @@ const authRouter = require('./routes/user');
 app.use('/auth', authRouter);
 
 // Task routes
-const taskRouter = require('./routes/tasks');
-app.use('/tasks', taskRouter);
+app.use('/tasks', TaskRouter); // Using task routes
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-//app.use(authRouter);
-
-//listen = express function
 http.listen(port, () => {
-    console.log('Listening at http://localhost:', port);
+    console.log('Server is running on port:', port);
 });
