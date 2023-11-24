4f55c5e92cd919fb82267f738fa96dc215727904
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-24 00:38:46 -0500

    Pushing the events backend code

--- Project/server/index.js
+++ Project/server/index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 8000;
 
 const User = require('./models/user');
 const TaskRouter = require('./routes/tasks'); // Importing task routes
+const EventRouter = require('./routes/events'); // Importing event routes
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,6 +20,9 @@ app.use('/auth', authRouter);
 
 // Task routes
 app.use('/tasks', TaskRouter); // Using task routes
+
+// Event routes
+app.use('/events', EventRouter); // Using event routes
 
 http.listen(port, () => {
     console.log('Server is running on port:', port);
