1ef068304191fec0a254cf7b7a9188bd1c35c318
Alexis <145714043+ChicoQuemador1@users.noreply.github.com>
2023-11-24 18:01:59 -0500

    Timeline w/ Add Task Button Revised 2
    Added a styling sheet for the modal, buttons, and text input fields.

--- Project/tinytask/app/(tabs)/CalendarScreen.tsx
+++ Project/tinytask/app/(tabs)/CalendarScreen.tsx
@@ -1,6 +1,13 @@
 import groupBy from 'lodash/groupBy';
 import React, { Component } from 'react';
-import { Button, TextInput, View } from 'react-native';
+import {
+  Button,
+  TextInput,
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+} from 'react-native';
 import Modal from 'react-native-modal';
 import {
   ExpandableCalendar,
@@ -26,141 +33,142 @@ const today = new Date();
 export const getDate = (offset = 0) =>
   CalendarUtils.getCalendarDateString(new Date().setDate(today.getDate() + offset));
 
-const timelineEvents: TimelineEventProps[] = [
-  {
-    start: `${getDate(-1)} 09:20:00`,
-    end: `${getDate(-1)} 12:00:00`,
-    title: 'Merge Request to React Native Calendars',
-    summary: 'Merge Timeline Calendar to React Native Calendars',
-  },
-  {
-    start: `${getDate()} 01:15:00`,
-    end: `${getDate()} 02:30:00`,
-    title: 'Meeting A',
-    summary: 'Summary for meeting A',
-    color: EVENT_COLOR,
-  },
-  {
-    start: `${getDate()} 01:30:00`,
-    end: `${getDate()} 02:30:00`,
-    title: 'Meeting B',
-    summary: 'Summary for meeting B',
-    color: EVENT_COLOR,
-  },
-  {
-    start: `${getDate()} 01:45:00`,
-    end: `${getDate()} 02:45:00`,
-    title: 'Meeting C',
-    summary: 'Summary for meeting C',
-    color: EVENT_COLOR,
-  },
-  {
-    start: `${getDate()} 02:40:00`,
-    end: `${getDate()} 03:10:00`,
-    title: 'Meeting D',
-    summary: 'Summary for meeting D',
-    color: EVENT_COLOR,
-  },
-  {
-    start: `${getDate()} 02:50:00`,
-    end: `${getDate()} 03:20:00`,
-    title: 'Meeting E',
-    summary: 'Summary for meeting E',
-    color: EVENT_COLOR,
-  },
-  {
-    start: `${getDate()} 04:30:00`,
-    end: `${getDate()} 05:30:00`,
-    title: 'Meeting F',
-    summary: 'Summary for meeting F',
-    color: EVENT_COLOR,
-  },
-  {
-    start: `${getDate(1)} 00:30:00`,
-    end: `${getDate(1)} 01:30:00`,
-    title: 'Visit Grand Mother',
-    summary: 'Visit Grand Mother and bring some fruits.',
-    color: 'lightblue',
-  },
-  {
-    start: `${getDate(1)} 02:30:00`,
-    end: `${getDate(1)} 03:20:00`,
-    title: 'Meeting with Prof. Behjet Zuhaira',
-    summary: 'Meeting with Prof. Behjet at 130 in her office.',
-    color: EVENT_COLOR,
-  },
-  {
-    start: `${getDate(1)} 04:10:00`,
-    end: `${getDate(1)} 04:40:00`,
-    title: 'Tea Time with Dr. Hasan',
-    summary: 'Tea Time with Dr. Hasan, Talk about Project',
-  },
-  {
-    start: `${getDate(1)} 01:05:00`,
-    end: `${getDate(1)} 01:35:00`,
-    title: 'Dr. Mariana Joseph',
-    summary: '3412 Piedmont Rd NE, GA 3032',
-  },
-  {
-    start: `${getDate(1)} 14:30:00`,
-    end: `${getDate(1)} 16:30:00`,
-    title: 'Meeting Some Friends in ARMED',
-    summary: 'Arsalan, Hasnaat, Talha, Waleed, Bilal',
-    color: 'pink',
-  },
-  {
-    start: `${getDate(2)} 01:40:00`,
-    end: `${getDate(2)} 02:25:00`,
-    title: 'Meet Sir Khurram Iqbal',
-    summary: 'Computer Science Dept. Comsats Islamabad',
-    color: 'orange',
-  },
-  {
-    start: `${getDate(2)} 04:10:00`,
-    end: `${getDate(2)} 04:40:00`,
-    title: 'Tea Time with Colleagues',
-    summary: 'WeRplay',
-  },
-  {
-    start: `${getDate(2)} 00:45:00`,
-    end: `${getDate(2)} 01:45:00`,
-    title: 'Lets Play Apex Legends',
-    summary: 'with Boys at Work',
-  },
-  {
-    start: `${getDate(2)} 11:30:00`,
-    end: `${getDate(2)} 12:30:00`,
-    title: 'Dr. Mariana Joseph',
-    summary: '3412 Piedmont Rd NE, GA 3032',
-  },
-  {
-    start: `${getDate(4)} 12:10:00`,
-    end: `${getDate(4)} 13:45:00`,
-    title: 'Merge Request to React Native Calendars',
-    summary: 'Merge Timeline Calendar to React Native Calendars',
-  },
-];
+  const timelineEvents: TimelineEventProps[] = [
+    {
+      start: `${getDate(-1)} 09:20:00`,
+      end: `${getDate(-1)} 12:00:00`,
+      title: 'Merge Request to React Native Calendars',
+      summary: 'Merge Timeline Calendar to React Native Calendars',
+    },
+    {
+      start: `${getDate()} 01:15:00`,
+      end: `${getDate()} 02:30:00`,
+      title: 'Meeting A',
+      summary: 'Summary for meeting A',
+      color: EVENT_COLOR,
+    },
+    {
+      start: `${getDate()} 01:30:00`,
+      end: `${getDate()} 02:30:00`,
+      title: 'Meeting B',
+      summary: 'Summary for meeting B',
+      color: EVENT_COLOR,
+    },
+    {
+      start: `${getDate()} 01:45:00`,
+      end: `${getDate()} 02:45:00`,
+      title: 'Meeting C',
+      summary: 'Summary for meeting C',
+      color: EVENT_COLOR,
+    },
+    {
+      start: `${getDate()} 02:40:00`,
+      end: `${getDate()} 03:10:00`,
+      title: 'Meeting D',
+      summary: 'Summary for meeting D',
+      color: EVENT_COLOR,
+    },
+    {
+      start: `${getDate()} 02:50:00`,
+      end: `${getDate()} 03:20:00`,
+      title: 'Meeting E',
+      summary: 'Summary for meeting E',
+      color: EVENT_COLOR,
+    },
+    {
+      start: `${getDate()} 04:30:00`,
+      end: `${getDate()} 05:30:00`,
+      title: 'Meeting F',
+      summary: 'Summary for meeting F',
+      color: EVENT_COLOR,
+    },
+    {
+      start: `${getDate(1)} 00:30:00`,
+      end: `${getDate(1)} 01:30:00`,
+      title: 'Visit Grand Mother',
+      summary: 'Visit Grand Mother and bring some fruits.',
+      color: 'lightblue',
+    },
+    {
+      start: `${getDate(1)} 02:30:00`,
+      end: `${getDate(1)} 03:20:00`,
+      title: 'Meeting with Prof. Behjet Zuhaira',
+      summary: 'Meeting with Prof. Behjet at 130 in her office.',
+      color: EVENT_COLOR,
+    },
+    {
+      start: `${getDate(1)} 04:10:00`,
+      end: `${getDate(1)} 04:40:00`,
+      title: 'Tea Time with Dr. Hasan',
+      summary: 'Tea Time with Dr. Hasan, Talk about Project',
+    },
+    {
+      start: `${getDate(1)} 01:05:00`,
+      end: `${getDate(1)} 01:35:00`,
+      title: 'Dr. Mariana Joseph',
+      summary: '3412 Piedmont Rd NE, GA 3032',
+    },
+    {
+      start: `${getDate(1)} 14:30:00`,
+      end: `${getDate(1)} 16:30:00`,
+      title: 'Meeting Some Friends in ARMED',
+      summary: 'Arsalan, Hasnaat, Talha, Waleed, Bilal',
+      color: 'pink',
+    },
+    {
+      start: `${getDate(2)} 01:40:00`,
+      end: `${getDate(2)} 02:25:00`,
+      title: 'Meet Sir Khurram Iqbal',
+      summary: 'Computer Science Dept. Comsats Islamabad',
+      color: 'orange',
+    },
+    {
+      start: `${getDate(2)} 04:10:00`,
+      end: `${getDate(2)} 04:40:00`,
+      title: 'Tea Time with Colleagues',
+      summary: 'WeRplay',
+    },
+    {
+      start: `${getDate(2)} 00:45:00`,
+      end: `${getDate(2)} 01:45:00`,
+      title: 'Lets Play Apex Legends',
+      summary: 'with Boys at Work',
+    },
+    {
+      start: `${getDate(2)} 11:30:00`,
+      end: `${getDate(2)} 12:30:00`,
+      title: 'Dr. Mariana Joseph',
+      summary: '3412 Piedmont Rd NE, GA 3032',
+    },
+    {
+      start: `${getDate(4)} 12:10:00`,
+      end: `${getDate(4)} 13:45:00`,
+      title: 'Merge Request to React Native Calendars',
+      summary: 'Merge Timeline Calendar to React Native Calendars',
+    },
+  ];   
 
 const INITIAL_TIME = { hour: 9, minutes: 0 };
 const EVENTS: TimelineEventProps[] = timelineEvents;
+
 export default class TimelineCalendarScreen extends Component<{}, State> {
   state: State = {
     currentDate: getDate(),
     events: EVENTS,
-    eventsByDate: groupBy(EVENTS, (e) => CalendarUtils.getCalendarDateString(e.start)) as {
-      [key: string]: TimelineEventProps[];
-    },
+    eventsByDate: groupBy(EVENTS, (e) =>
+      CalendarUtils.getCalendarDateString(e.start)
+    ) as { [key: string]: TimelineEventProps[] },
     newEventTitle: '',
     newEventSummary: '',
     isModalVisible: false,
   };
 
   marked = {
-    [`${getDate(-1)}`]: {marked: true},
-    [`${getDate()}`]: {marked: true},
-    [`${getDate(1)}`]: {marked: true},
-    [`${getDate(2)}`]: {marked: true},
-    [`${getDate(4)}`]: {marked: true}
+    [`${getDate(-1)}`]: { marked: true },
+    [`${getDate()}`]: { marked: true },
+    [`${getDate(1)}`]: { marked: true },
+    [`${getDate(2)}`]: { marked: true },
+    [`${getDate(4)}`]: { marked: true },
   };
 
   onDateChanged = (date: string, source: string) => {
@@ -211,7 +219,13 @@ export default class TimelineCalendarScreen extends Component<{}, State> {
   };
 
   render() {
-    const { currentDate, eventsByDate, newEventTitle, newEventSummary, isModalVisible } = this.state;
+    const {
+      currentDate,
+      eventsByDate,
+      newEventTitle,
+      newEventSummary,
+      isModalVisible,
+    } = this.state;
 
     return (
       <CalendarProvider
@@ -219,14 +233,12 @@ export default class TimelineCalendarScreen extends Component<{}, State> {
         onDateChanged={this.onDateChanged}
         onMonthChange={this.onMonthChange}
         showTodayButton
-        disabledOpacity={0.6}
-      >
-        <View style={{ marginVertical: 8, padding: 8, borderRadius: 5, backgroundColor: '#f194ff' }}>
+        disabledOpacity={0.6}>
+        <View style={styles.addButtonContainer}>
           <Button
             title="Add Event"
             color="white"
             onPress={() => this.setState({ isModalVisible: true })}
-           
           />
         </View>
         <ExpandableCalendar
@@ -234,45 +246,113 @@ export default class TimelineCalendarScreen extends Component<{}, State> {
           hideArrows
           markedDates={this.marked}
         />
-      <TimelineList
+        <TimelineList
           events={eventsByDate}
           timelineProps={this.timelineProps}
           showNowIndicator
           scrollToFirst
           initialTime={INITIAL_TIME}
         />
-<Modal
-  testID={'modal'}
-  isVisible={isModalVisible}
-  backdropColor="#B4B3DB"
-  backdropOpacity={0.8}
-  animationIn="zoomInDown"
-  animationOut="zoomOutUp"
-  animationInTiming={600}
-  animationOutTiming={600}
-  backdropTransitionInTiming={600}
-  backdropTransitionOutTiming={600}
-  style={{ margin: 0 }}>
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <View style={{ width: 300, padding: 20, backgroundColor: 'white', borderRadius: 10 }}>
-      <TextInput
-        placeholder="Event Title"
-        value={newEventTitle}
-        onChangeText={(text) => this.setState({ newEventTitle: text })}
-        style={{ borderWidth: 1, marginBottom: 10, padding: 5 }}
-      />
-      <TextInput
-        placeholder="Event Summary"
-        value={newEventSummary}
-        onChangeText={(text) => this.setState({ newEventSummary: text })}
-        style={{ borderWidth: 1, marginBottom: 10, padding: 5 }}
-      />
-      <Button title="Create" onPress={this.handleCreateEvent} />
-      <Button title="Cancel" onPress={() => this.setState({ isModalVisible: false })} />
-    </View>
-  </View>
-</Modal>
+        <Modal
+          testID={'modal'}
+          isVisible={isModalVisible}
+          backdropColor="#B4B3DB"
+          backdropOpacity={0.8}
+          animationIn="zoomInDown"
+          animationOut="zoomOutUp"
+          animationInTiming={600}
+          animationOutTiming={600}
+          backdropTransitionInTiming={600}
+          backdropTransitionOutTiming={600}
+          style={styles.modal}>
+          
+          <View style={styles.modalContent}>
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Title</Text>
+              <TextInput
+                value={newEventTitle}
+                onChangeText={(text) => this.setState({ newEventTitle: text })}
+                style={styles.inputEvent}
+              />
+              <Text style={styles.label}>Summary</Text>
+              <TextInput
+                value={newEventSummary}
+                onChangeText={(text) => this.setState({ newEventSummary: text })}
+                style={styles.inputSummary}
+              />
+            <TouchableOpacity
+              style={styles.createButton}
+              onPress={this.handleCreateEvent}>
+              <Text style={styles.buttonText}>Create</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.cancelButton}
+              onPress={() => this.setState({ isModalVisible: false })}>
+              <Text style={styles.buttonText}>Cancel</Text>
+            </TouchableOpacity>
+            </View>
+            </View>
+        </Modal>
       </CalendarProvider>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  modal: {
+    margin: 0,
+  },
+  addButtonContainer: {
+    marginVertical: 8,
+    padding: 8,
+    borderRadius: 5,
+    backgroundColor: "#007BFF",
+  },
+  modalContent: {
+    flex: 1, justifyContent: 'center', alignItems: 'center' 
+  },
+  formGroup: {
+    width: 300, padding: 20, backgroundColor: 'white', borderRadius: 10
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+    color: 'black',
+  },
+  inputEvent: {
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 5,
+  },
+  inputSummary: {
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 5,
+    height: 100
+  },
+  createButton: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  cancelButton: {
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
