dd968ec32f05c644b3abe07300ca19a58fbd3d11
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-24 19:21:20 -0500

    Fixed the events backend code

--- Project/server/routes/events.js
+++ Project/server/routes/events.js
@@ -1,25 +1,11 @@
+// Project\server\routes\events.js
 const express = require('express');
 const router = express.Router();
-const Event = require('../models/event');
+const { createEvent, getEvents, deleteEvent } = require('../controllers/event');
 
-// Route to create a new event
-router.post('/events', async (req, res) => {
-    try {
-        const { name, notes, start_time, end_time } = req.body;
-
-        const newEvent = await Event.create({
-            name,
-            notes,
-            start_time,
-            end_time,
-        });
-
-        res.status(201).json(newEvent);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-// add the other routes related to events can be added here
+router.post('/', createEvent);
+router.get('/', getEvents);
+router.delete('/:id', deleteEvent);
+// Additional routes can be added for updating events
 
 module.exports = router;
