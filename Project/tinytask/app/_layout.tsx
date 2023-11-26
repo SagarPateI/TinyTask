b08eb44b1c3de689b0c31ef1102ba84105e9f380
cristalc12 <cristalc12@gmail.com>
2023-11-25 22:05:21 -0500

    changed login and signup ui color (navigation broken

--- Project/tinytask/app/_layout.tsx
+++ Project/tinytask/app/_layout.tsx
@@ -54,6 +54,7 @@ function RootLayoutNav() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        
       </Stack>
     </ThemeProvider>
   );
