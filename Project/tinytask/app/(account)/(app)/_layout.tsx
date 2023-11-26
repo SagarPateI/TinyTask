349079e83eb1348323145e6f98366e5220b7d89d
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-25 21:06:45 -0500

    Fixed the font directory being misconfigured in the last push.

--- Project/tinytask/app/(account)/(app)/_layout.tsx
+++ Project/tinytask/app/(account)/(app)/_layout.tsx
@@ -24,7 +24,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require("../../../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
