e122c46c9004056a25c8986bbfd4c0fa7754b846
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-25 22:46:16 -0500

    Massive restructuring of the file system and how the app's navigation system works

--- Project/tinytask/app/_layout.tsx
+++ Project/tinytask/app/_layout.tsx
@@ -16,7 +16,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
+  initialRouteName: "(account)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -52,7 +52,7 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(accounts)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
     </ThemeProvider>
