409f61a13bd8d293c181e1059fe7c777a4381bff
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-26 13:31:40 -0500

    Implemented my dark theme from before

--- Project/tinytask/app/_layout.tsx
+++ Project/tinytask/app/_layout.tsx
@@ -3,19 +3,29 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import AccountLayout from "./(account)/_layout";
+import { useColorScheme } from "react-native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 
 const Stack = createStackNavigator();
 
 const RootLayout = () => {
+  const colorScheme = useColorScheme();
+
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="(account)"
-        component={AccountLayout}
-        options={{ headerShown: false }}
-      />
-      {/* Add more screens or navigation configurations as needed */}
-    </Stack.Navigator>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="(account)"
+          component={AccountLayout}
+          options={{ headerShown: false }}
+        />
+        {/* Add more screens or navigation configurations as needed */}
+      </Stack.Navigator>
+    </ThemeProvider>
   );
 };
 
