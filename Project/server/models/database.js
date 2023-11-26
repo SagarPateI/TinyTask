707ab1d0a8c5e3a4488de5ac4f00cc444c048018
cris <143109153+cristalc13@users.noreply.github.com>
2023-11-26 18:04:23 -0500

    Delete Project/server directory

--- Project/server/models/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URI)
-        .then(() => console.log("DB connected"))
-        .catch((err) => console.log("DB connection error: ", err));
