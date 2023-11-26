4a7ba8a6347f77135db591f426bd4a62e3a84b4f
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-26 14:52:01 -0500

    Fixed the weird font issue, as far as I can tell

--- Project/tinytask/app/_layout.tsx
+++ Project/tinytask/app/_layout.tsx
@@ -1,44 +1,33 @@
-// Project\tinytask\app\_layout.tsx
-
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import AccountLayout from "./(account)/_layout";
-import { useColorScheme } from "react-native";
+import { ActivityIndicator, View, useColorScheme } from "react-native";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
+import * as SplashScreen from "expo-splash-screen";
 import { useFonts } from "expo-font";
-import { SplashScreen } from "expo-router";
-import { useEffect, useState } from "react";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 const Stack = createStackNavigator();
 
 const RootLayout = () => {
-  useEffect(() => {
-    SplashScreen.preventAutoHideAsync();
-  }, []);
-
-  const SpaceMonoRegular = require("../assets/fonts/SpaceMono-Regular.ttf");
-  const [loaded, error] = useFonts({
-    SpaceMono: SpaceMonoRegular, // Use the imported font directly
-    ...FontAwesome.font,
-  });
-  //const SpaceMonoRegular = require("../assets/fonts/SpaceMono-Regular.ttf");
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded || error) {
-    console.log("ERROR: The font was not loaded.");
-  }
-
   const colorScheme = useColorScheme();
+
+  const [fontsLoaded] = useFonts({
+    'SpaceMono': require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
