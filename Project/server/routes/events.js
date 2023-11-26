558f8c5a3aeabf7b33aacafac3c33bb7a65b2b49
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-26 15:32:55 -0500

    Added rate limits for the events.js routes

--- Project/server/routes/events.js
+++ Project/server/routes/events.js
@@ -1,11 +1,39 @@
-// Project\server\routes\events.js
 const express = require('express');
+const { RateLimiterMemory } = require('rate-limiter-flexible');
+
+const limiter = new RateLimiterMemory({
+    points: 5,
+    duration: 1,
+});
+
 const router = express.Router();
 const { createEvent, getEvents, deleteEvent } = require('../controllers/event');
 
-router.post('/', createEvent);
-router.get('/', getEvents);
-router.delete('/:id', deleteEvent);
-// Additional routes can be added for updating events
+router.post('/', async (req, res, next) => {
+    try {
+        await limiter.consume(req.ip);
+        next();
+    } catch (error) {
+        res.status(429).send('Too many requests');
+    }
+}, createEvent);
+
+router.get('/', async (req, res, next) => {
+    try {
+        await limiter.consume(req.ip);
+        next();
+    } catch (error) {
+        res.status(429).send('Too many requests');
+    }
+}, getEvents);
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        await limiter.consume(req.ip);
+        next();
+    } catch (error) {
+        res.status(429).send('Too many requests');
+    }
+}, deleteEvent);
 
 module.exports = router;
