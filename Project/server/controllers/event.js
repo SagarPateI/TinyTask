304167aec35c93fdf1627ef3a319e96d2f1f679b
Alexis <145714043+ChicoQuemador1@users.noreply.github.com>
2023-11-27 03:34:18 -0500

    Timeline w/ DB

--- Project/server/controllers/event.js
+++ Project/server/controllers/event.js
@@ -1,15 +1,17 @@
 // Project\server\controllers\event.js
+
 const Event = require('../models/event');
 
 exports.createEvent = async (req, res) => {
   try {
+    console.log('Request body:', req.body); // Log the request body to see if data is received correctly
     const { start, end, title, summary, color } = req.body;
     const newEvent = new Event({ start, end, title, summary, color });
     await newEvent.save(); // Save to MongoDB
+    console.log('Event created:', newEvent); // Log the created event
     res.status(201).json(newEvent);
   } catch (error) {
     console.error('Error creating event:', error);
     res.status(500).json({ error: error.message });
   }
 };
-
