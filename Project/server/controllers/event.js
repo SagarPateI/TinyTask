fdc306cf11bf74145847120b492766b38e8bc755
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-27 03:55:57 -0500

    Changed the backend code for the events to get it working

--- Project/server/controllers/event.js
+++ Project/server/controllers/event.js
@@ -2,16 +2,48 @@
 
 const Event = require('../models/event');
 
+// exports.createEvent = async (req, res) => {
+//   try {
+//     console.log('Request body:', req.body); // Log the request body to see if data is received correctly
+//     const { start, end, title, summary, color } = req.body;
+//     const newEvent = new Event({ start, end, title, summary, color });
+//     await newEvent.save(); // Save to MongoDB
+//     console.log('Event created:', newEvent); // Log the created event
+//     res.status(201).json(newEvent);
+//   } catch (error) {
+//     console.error('Error creating event:', error);
+//     res.status(500).json({ error: error.message });
+//   }
+// };
+
 exports.createEvent = async (req, res) => {
   try {
-    console.log('Request body:', req.body); // Log the request body to see if data is received correctly
     const { start, end, title, summary, color } = req.body;
     const newEvent = new Event({ start, end, title, summary, color });
-    await newEvent.save(); // Save to MongoDB
-    console.log('Event created:', newEvent); // Log the created event
+    await newEvent.save();
     res.status(201).json(newEvent);
   } catch (error) {
-    console.error('Error creating event:', error);
     res.status(500).json({ error: error.message });
   }
 };
+
+exports.getEvents = async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.deleteEvent = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Event.findByIdAndDelete(id);
+    res.json({ message: 'Event deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Additional controller methods for events can also be defined here
