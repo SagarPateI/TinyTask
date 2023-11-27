e4701a823eea9930bae85d58a11a8701651ac526
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-27 04:59:51 -0500

    Revert "Revert "Implemented my fixes for the events backend code.""
    This reverts commit 8265f18dd777898e0ee8690fd5d2c07fb79ef2b3.

--- Project/server/routes/events.js
+++ Project/server/routes/events.js
@@ -2,25 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
-const { createEvent, getEvents } = require('../controllers/event');
-const { RateLimiterMemory } = require('rate-limiter-flexible');
+const { createEvent, getEvents, deleteEvent } = require('../controllers/event');
 
-const limiter = new RateLimiterMemory({
-    points: 5,
-    duration: 1,
-});
-
-const rateLimiterMiddleware = async (req, res, next) => {
-    try {
-        await limiter.consume(req.ip);
-        next();
-    } catch (error) {
-        res.status(429).send('Too many requests');
-    }
-};
-
-router.post('/', rateLimiterMiddleware, createEvent);
+router.post('/', createEvent);
 router.get('/', getEvents);
-// Additional routes for events can be added here
+router.delete('/:id', deleteEvent);
+// Additional routes can be added for updating tasks
 
 module.exports = router;
