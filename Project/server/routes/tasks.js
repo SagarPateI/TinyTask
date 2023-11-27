d696e91f04379587b58fd374ef39b57d0533d6eb
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-27 00:08:52 -0500

    Added a new rate limit which does not break the functionality.

--- Project/server/routes/tasks.js
+++ Project/server/routes/tasks.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const rateLimit = require('express-rate-limit');
 const { createTask, getTasks, deleteTask } = require('../controllers/task');
 
-router.post('/', createTask);
-router.get('/', getTasks);
-router.delete('/:id', deleteTask);
+// Set up rate limiting middleware
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute window
+    max: 5, // 5 requests per windowMs
+    message: 'Too many requests from this IP, please try again later.',
+});
+
+// Apply rate limiting middleware to specific routes
+router.post('/', limiter, createTask);
+router.get('/', limiter, getTasks);
+router.delete('/:id', limiter, deleteTask);
+
 // Additional routes can be added for updating tasks
 
 module.exports = router;
