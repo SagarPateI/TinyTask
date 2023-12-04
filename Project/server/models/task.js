91d28a31076ffc23f5c716e3fc634c2e60a9059a
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-12-04 18:48:36 -0500

    Committing my fixes for the task screen user ID issue.

--- Project/server/models/task.js
+++ Project/server/models/task.js
@@ -1,27 +1,25 @@
 // Project\server\models\task.js
 const mongoose = require('mongoose');
+const User = require('./user'); // Import the User model (Update the path as needed)
 
 const taskSchema = new mongoose.Schema({
-
     title: {
         type: String,
         required: true,
     },
-
     // description: {
     //     type: String,
     //     required: true,
     // },
-
     completed: {
         type: Boolean,
         default: false,
     },
     userId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
+        ref: 'User', // Referring to the User model
     },
 });
 
-const taskModel = mongoose.model('tasks', taskSchema);
+const taskModel = mongoose.model('Task', taskSchema);
 module.exports = taskModel;
