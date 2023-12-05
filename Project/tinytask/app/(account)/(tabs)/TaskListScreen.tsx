180ef55946d2288d01d0e05d65a809688e2afe50
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-12-05 01:13:06 -0500

    Fixed the aesthetics of the TaskListScreen boxes and added back the ability to pull and view tasks

--- Project/tinytask/app/(account)/(tabs)/TaskListScreen.tsx
+++ Project/tinytask/app/(account)/(tabs)/TaskListScreen.tsx
@@ -1,223 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import { useNavigation } from "@react-navigation/native";
-// import { StackNavigationProp } from "@react-navigation/stack";
-// import {
-//   Keyboard,
-//   KeyboardAvoidingView,
-//   StyleSheet,
-//   Text,
-//   View,
-//   TextInput,
-//   TouchableOpacity,
-//   Platform,
-//   ScrollView,
-// } from "react-native";
-// import Task from "../../../components/Task";
-// import {
-//   Text as ThemedText,
-//   View as ThemedView,
-//   useThemeColor,
-// } from "../../../components/Themed";
-// import { useQuery } from "react-query";
-// import axios from 'axios';
-
-// interface Task {
-//   _id: string;
-//   title: string;
-//   completed: boolean;
-// }
-
-// interface TaskListProps {
-//   task: Task[];
-// }
-
-// const TaskListScreen: React.FC = () => {
-//   const navigation = useNavigation<StackNavigationProp<any>>();
-
-//   const [task, setTask] = useState<Task[]>([]);
-
-//   useEffect(() => {
-//     axios
-//       .get("https://tinytask.loca.lt/tasks", {
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       })
-//       .then((res) => {
-//         if (res.status === 200) {
-//           setTask(res.data.task);
-//         }
-//       });
-//   }, []);
-
-//   const TaskList = ({task}: TaskListProp) => {
-
-//   }
-//   // const GetTasks = async () => {
-//   //   try {
-//   //     const response = await fetch("https://tinytask.loca.lt/tasks", {
-//   //       method: "GET",
-//   //       headers: {
-//   //         "Content-Type": "application/json",
-//   //       },
-//   //     });
-//   //     const data = await response.json();
-//   //     setTask(data);
-//   //   } catch (error) {
-//   //     console.error("Error fetching tasks:", error);
-//   //   }
-//   // };
-//   //STATE VARIABLES
-//   // const [task, setTask] = useState([]);
-//   // const [taskItems, setTaskItems] = useState<string[]>([]);
-
-//   const backgroundColor = useThemeColor(
-//     {
-//       light: "#FFFFFF", // Light gray background color
-//       dark: "#121212", // Dark gray background color
-//     },
-//     "background"
-//   );
-
-//   // //ADD TASK FUNCTION
-//   // const handleAddTask = async () => {
-//   //   Keyboard.dismiss();
-
-//   //   //IF TASK IS NOT EMPTY
-//   //   if (task () !== "") {
-//   //     try {
-//   //       //POST REQUEST TO MAKE TASK
-//   //       const response = await fetch("http://localhost:8000/tasks", {
-//   //         method: "POST",
-//   //         headers: {
-//   //           "Content-Type": "application/json",
-//   //         },
-//   //         body: JSON.stringify({
-//   //           title: task,
-//   //           description: "Your task description here", // Add description as needed
-//   //         }),
-//   //       });
-
-//   //       //IF REQUEST SUCCESSFUL, UPDATE THE STATE WITH NEW TASK
-//   //       if (response.ok) {
-//   //         const newTask = await response.json();
-//   //         setTaskItems([...taskItems, newTask.title]); // Update taskItems with the new task
-//   //         setTask("");
-//   //         console.log("Task added successfully:", newTask); // Log success message
-//   //       } else {
-//   //         console.error("Failed to add task"); // Log failure message
-//   //       }
-//   //     } catch (error) {
-//   //       console.error("Error:", error); // Log error message
-//   //     }
-//   //   }
-//   // };
-
-//   // //FUNCTION TO SET TASK AS COMPLETE
-//   // const completeTask = (index: number) => {
-//   //   let itemsCopy = [...task];
-//   //   itemsCopy.splice(index, 1);
-//   //   setTask(itemsCopy);
-//   // };
-
-//   const textColor = useThemeColor({}, "text");
-
-//   const styles = StyleSheet.create({
-//     container: {
-//       flex: 1,
-//       paddingHorizontal: 20,
-//       backgroundColor,
-//     },
-//     tasksWrapper: {
-//       paddingTop: 80,
-//     },
-//     sectionTitle: {
-//       fontSize: 24,
-//       fontWeight: "bold",
-//       color: textColor,
-//     },
-//     items: {
-//       marginTop: 30,
-//     },
-//     writeTaskWrapper: {
-//       position: "absolute",
-//       bottom: 60,
-//       width: "100%",
-//       flexDirection: "row",
-//       justifyContent: "space-around",
-//       alignItems: "center",
-//     },
-//     input: {
-//       paddingVertical: 15,
-//       paddingHorizontal: 15,
-//       borderRadius: 10,
-//       borderColor: "#C0C0C0",
-//       borderWidth: 1,
-//       width: 250,
-//       color: textColor, // Applying Themed color
-//     },
-//     addWrapper: {
-//       width: 60,
-//       height: 60,
-//       borderRadius: 10,
-//       justifyContent: "center",
-//       alignItems: "center",
-//       borderColor: "#C0C0C0",
-//       borderWidth: 1,
-//     },
-//     addText: {
-//       color: textColor,
-//       fontSize: 30,
-//     },
-//     scrollContainer: {
-//       flexGrow: 1,
-//     },
-//   });
-
-//   const TaskListComponent = ({task}: TaskListProps) => {
-
-//   return (
-//     <ThemedView style={styles.container}>
-//       <ScrollView
-//         contentContainerStyle={styles.scrollContainer}
-//         keyboardShouldPersistTaps="handled"
-//       >
-//         <View style={styles.tasksWrapper}>
-//           <ThemedText style={styles.sectionTitle}>Today's tasks</ThemedText>
-//           <View style={styles.items}>
-//             {task.map((item, index) => (
-//               <TouchableOpacity key={index} onPress={() => completeTask(index)}>
-//                 <Task text={item} />
-//               </TouchableOpacity>
-//             ))}
-//           </View>
-//         </View>
-//       </ScrollView>
-
-//       {/* Write a task */}
-//       {/* <KeyboardAvoidingView
-//         behavior={Platform.OS === "ios" ? "padding" : "height"}
-//         style={styles.writeTaskWrapper}
-//       >
-//         <TextInput
-//           style={styles.input}
-//           placeholder={"Write a task"}
-//           placeholderTextColor={textColor}
-//           value={task}
-//           onChangeText={(text) => setTask(text)}
-//         />
-//         <TouchableOpacity onPress={handleAddTask}>
-//           <View style={styles.addWrapper}>
-//             <Text style={styles.addText}>+</Text>
-//           </View>
-//         </TouchableOpacity>
-//       </KeyboardAvoidingView> */}
-//     </ThemedView>
-//   );
-//     }
-
-// export default TaskListScreen;
-
 import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
@@ -245,22 +25,16 @@ interface TaskItem {
 }
 
 const TaskListScreen: React.FC = () => {
-  const navigation = useNavigation();
   const [tasks, setTasks] = useState<TaskItem[]>([]);
-  const [title, setTitle] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    fetchTasks(); // Fetch tasks when the component mounts
+    fetchTasks();
   }, []);
 
-  // Inside fetchTasks function
   const fetchTasks = async () => {
     try {
-      console.log("Fetching tasks..."); // Add this log
       const response = await axios.get("https://tinytaskapp.loca.lt/tasks");
       if (response.status === 200) {
-        console.log("Tasks fetched successfully:", response.data); // Add this log
         setTasks(response.data);
       }
     } catch (error) {
@@ -268,26 +42,38 @@ const TaskListScreen: React.FC = () => {
     }
   };
 
-  // Inside handleAddTask function
-const handleAddTask = async () => {
-  setLoading(true);
-  try {
-    const userId = "6553cbce9c2e8667ef0ce643"; // Replace this with the actual user ID
-    const response = await axios.post("https://tinytaskapp.loca.lt/tasks", {
-      title: title,
-      completed: false,
-      userId: userId, // Include the user ID in the request body
-    });
-    if (response.status === 201) {
-      setTasks([...tasks, response.data]);
-      setTitle("");
-      console.log("Task added successfully:", response.data);
+  const handleAddTask = async () => {
+    try {
+      const userId = "6553cbce9c2e8667ef0ce643";
+      const response = await axios.post("https://tinytaskapp.loca.lt/tasks", {
+        title: title,
+        completed: false,
+        userId: userId,
+      });
+      if (response.status === 201) {
+        setTasks([...tasks, response.data]);
+        setTitle("");
+      }
+    } catch (error) {
+      console.error("Error adding task:", error);
     }
-  } catch (error) {
-    setLoading(false);
-    console.error("Error adding task:", error);
-  }
-};
+  };
+
+  const handleTaskPress = async (taskId: string) => {
+    try {
+      const updatedTasks = tasks.map((task) => {
+        if (task._id === taskId) {
+          task.completed = true;
+        }
+        return task;
+      });
+      setTasks(updatedTasks);
+
+      await axios.delete(`https://tinytaskapp.loca.lt/tasks/${taskId}`);
+    } catch (error) {
+      console.error("Error updating task:", error);
+    }
+  };
 
   const textColor = useThemeColor({}, "text");
 
@@ -295,6 +81,7 @@ const handleAddTask = async () => {
     container: {
       flex: 1,
       paddingHorizontal: 20,
+      paddingTop: 40,
       backgroundColor: useThemeColor(
         { light: "#FFFFFF", dark: "#121212" },
         "background"
@@ -311,7 +98,6 @@ const handleAddTask = async () => {
     items: {
       marginTop: 30,
     },
-
     writeTaskWrapper: {
       position: "absolute",
       bottom: 60,
@@ -320,7 +106,6 @@ const handleAddTask = async () => {
       justifyContent: "space-around",
       alignItems: "center",
     },
-
     input: {
       paddingVertical: 15,
       backgroundColor: "black",
@@ -331,7 +116,6 @@ const handleAddTask = async () => {
       width: 250,
       color: textColor,
     },
-
     addWrapper: {
       width: 60,
       height: 60,
@@ -341,15 +125,22 @@ const handleAddTask = async () => {
       borderColor: "#C0C0C0",
       borderWidth: 1,
     },
-
     addText: {
       color: textColor,
       fontSize: 30,
     },
-
     scrollContainer: {
       flexGrow: 1,
     },
+    taskItem: {
+      marginBottom: 10,
+    },
+    // addButton: {
+    //   backgroundColor: "blue",
+    //   padding: 10,
+    //   alignItems: "center",
+    //   borderRadius: 5,
+    // },
     addButton: {
       backgroundColor: "blue",
       justifyContent: "center",
@@ -358,36 +149,28 @@ const handleAddTask = async () => {
       height: 50,
       borderRadius: 25,
     },
+    addButtonText: {
+      color: "white",
+    },
   });
 
   // JSX Component
   return (
     <View style={styles.container}>
       <ScrollView>
-        <Text style={styles.sectionTitle}>Today's tasks</Text>
-        {tasks.map((task) => (
-          <TouchableOpacity key={task._id}>
-            <Text>{task.title}</Text>
-          </TouchableOpacity>
-        ))}
+        {tasks.map(
+          (task) =>
+            !task.completed && (
+              <TouchableOpacity
+                key={task._id}
+                style={styles.taskItem}
+                onPress={() => handleTaskPress(task._id)}
+              >
+                <Task text={task.title} />
+              </TouchableOpacity>
+            )
+        )}
       </ScrollView>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
-      >
-        <TextInput
-          style={styles.input}
-          placeholder={"Write a task"}
-          value={title}
-          onChangeText={(text) => setTitle(text)}
-        />
-
-        <TouchableOpacity onPress={handleAddTask}>
-          <View style={styles.addWrapper}>
-            <Text style={styles.addText}>+</Text>
-          </View>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
     </View>
   );
 };
