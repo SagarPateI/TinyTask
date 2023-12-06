3f9695eafb030d453f01823a16fad95ebdc3eebe
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-12-05 19:09:43 -0500

    Added the code to get the user ID

--- Project/tinytask/app/(account)/services/AuthService.ts
+++ Project/tinytask/app/(account)/services/AuthService.ts
@@ -1,6 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const TOKEN_KEY = 'user_token';
+const USER_ID = 'user_id';
 
 export const AuthService = {
   // Saves user token to AsyncStorage
@@ -34,6 +35,40 @@ export const AuthService = {
     } catch (error) {
       console.error('Failed to clear token:', error);
       throw new Error('Failed to clear token');
+    }
+  },
+
+  // Saves user ID to AsyncStorage
+  async saveID(userId: string): Promise<void> {
+    try {
+      await AsyncStorage.setItem(USER_ID, userId);
+      console.log('User ID saved successfully:', userId);
+    } catch (error) {
+      console.error('Failed to save user ID:', error);
+      throw new Error('Failed to save user ID');
+    }
+  },
+
+  // Retrieves user ID from AsyncStorage
+  async getID(): Promise<string | null> {
+    try {
+      const userId = await AsyncStorage.getItem(USER_ID);
+      console.log('Retrieved user ID:', userId);
+      return userId;
+    } catch (error) {
+      console.error('Failed to get user ID:', error);
+      throw new Error('Failed to get user ID');
+    }
+  },
+
+  // Clears user ID from AsyncStorage
+  async clearID(): Promise<void> {
+    try {
+      await AsyncStorage.removeItem(USER_ID);
+      console.log('User ID cleared successfully');
+    } catch (error) {
+      console.error('Failed to clear user ID:', error);
+      throw new Error('Failed to clear user ID');
     }
   },
 
